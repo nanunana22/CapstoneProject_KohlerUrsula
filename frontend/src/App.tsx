03898786874f95a70fcccae948029d0ca850b389
ursulakohler <kohlerursula22@gmail.com>
2024-09-03 16:45:58 +0200

    add RidinglessonColumn frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import {Ridinglesson} from "./models/Ridinglesson.ts";
 import RidinglessonColumn from "./RidinglessonColumn.tsx";
 import {allPossibleRidinglessons} from "./RidinglessonStatus.ts";
+import RidinglessonCard from "./components/RidinglessonCard.tsx";
 
 
 export default function App() {
@@ -21,16 +22,26 @@ export default function App() {
     if (!ridinglessons){
         return "Please wait =)..."
     }
+    console.log(ridinglessons)
   return (
       <>
           <h1>Riding lesson booking system</h1>
-          {
-              allPossibleRidinglessons.map(status => {
-                  const filteredRidinglessons =
-                      ridinglessons.filter(ridinglessons => ridinglessons.status === status);
-                  return <RidinglessonColumn status={status} ridinglessons={filteredRidinglessons} />})
-          }
-          </>
+          <h2>TO_CREATE</h2>
+          <ul>
+              {ridinglessons.filter(lesson => lesson.status == "TO_CREATE").map((ridinglesson, index) =>
+                  <RidinglessonCard key={ridinglesson.id} ridinglesson={ridinglesson}/>)}
+          </ul>
+          <h2>TO_BOOK</h2>
+          <ul>
+              {ridinglessons.filter(lesson => lesson.status == "TO_BOOK").map((ridinglesson, index) =>
+                  <RidinglessonCard key={ridinglesson.id} ridinglesson={ridinglesson}/>)}
+          </ul>
+          <h2>BOOKED_LESSON</h2>
+          <ul>
+              {ridinglessons.filter(lesson => lesson.status == "BOOKED_LESSON").map((ridinglesson, index) =>
+                  <RidinglessonCard key={ridinglesson.id} ridinglesson={ridinglesson}/>)}
+          </ul>
+      </>
   )
 }
 
