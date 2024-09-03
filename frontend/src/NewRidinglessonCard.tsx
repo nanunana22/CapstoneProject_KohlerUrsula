18dadf14654286ae645ccb3c5624cd6a8e89c691
ursulakohler <kohlerursula22@gmail.com>
2024-09-03 18:03:56 +0200

    add save button

--- frontend/src/NewRidinglessonCard.tsx
+++ frontend/src/NewRidinglessonCard.tsx
@@ -5,6 +5,7 @@ export default function NewRidinglessonCard(props: Props): JSX.Element {
     return(
         <div className="ridinglesson-card new-ridinglesson">
             <input type="text" />
+            <button>save</button>
         </div>
     );
 }
