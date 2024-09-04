ec928299744784c915d1feec341baa8873c43942
ursulakohler <kohlerursula22@gmail.com>
2024-09-04 10:44:26 +0200

    add save/post function

--- frontend/src/NewRidinglessonCard.tsx
+++ frontend/src/NewRidinglessonCard.tsx
@@ -1,11 +1,31 @@
+import {useState} from "react";
+import {Ridinglesson} from "./models/Ridinglesson.ts";
+import axios from "axios";
+
 type Props = {
 }
 
 export default function NewRidinglessonCard(props: Props): JSX.Element {
+
+    const[text, setText ] = useState('');
+    function changeText(event: React.ChangeEvent<HTMLInputElement>) {
+        setText(event.target.value)
+    }
+
+    function saveRidinglesson(){
+        axios.post("/api/ridinglessons", {
+            ridinginstructor: text,
+            ridingtype: text,
+            horse: text,
+            time: text,
+            date: text,
+            status: "TO_BOOK"
+        } as Ridinglesson)
+    }
     return(
         <div className="ridinglesson-card new-ridinglesson">
-            <input type="text" />
-            <button>save</button>
+            <input type="text" onInput={changeText}/>
+            <button onClick={saveRidinglesson}>save</button>
         </div>
     );
 }
