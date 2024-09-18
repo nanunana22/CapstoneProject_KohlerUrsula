85a45542f3b191c46712c5e6916161e422b457d8
ursulakohler <kohlerursula22@gmail.com>
2024-09-18 15:51:12 +0200

    add frontend for OAUTH

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,24 +4,29 @@ import axios from "axios";
 import {Ridinglesson} from "./models/Ridinglesson.ts";
 import RidinglessonColumn from "./RidinglessonColumn.tsx";
 import {allPossibleRidinglessons} from "./RidinglessonStatus.ts";
+import NewRidinglessonCard from "./NewRidinglessonCard.tsx";
 
 
 
 export default function App() {
 
     const [ridinglessons, setRidinglesson] = useState<Ridinglesson[]>()
+    const [user, setUser] = useState<String>()
+
+    useEffect(() =>{
+        getUser()
+    }, []);
 
     function login() {
         const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
-        console.log(host, "!!!")
         window.open(host + '/oauth2/authorization/github', '_self')
-
     }
 
     function getUser(){
         axios.get("api/users/me")
             .then((response) => {
                 console.log(response.data)
+                setUser(response.data)
             })
     }
 
@@ -53,6 +58,14 @@ export default function App() {
           <img width={200} src="/src/Asmano.jpg" alt={"not found"}/>
           <button onClick={login}>Login</button>
           <button onClick={getUser}>Me</button>
+          <p>{user}</p>
+          <Routes>
+              <Route path={"ridinglessons"} element={<RidinglessonColumn status={status}
+                                                                         ridinglessons={ridinglessons}
+                                                                         onNewRidinglessonItemSaved={}
+                                                                         deleteData={deleteRidinglesson}/>}/>
+              <Route path={"/ridinglessons/getAllLessons"} element={<NewRidinglessonCard onNewRidinglessonSaved={} }
+          </Routes>
           {
               allPossibleRidinglessons.map(status => {
                   const filteredRidinglessons: Ridinglesson[] =
